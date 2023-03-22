d102b10fa66a6311ae6745cce0922118a0e162c1
Sivert Fjeldstad Madsen <sivertfm@gmail.com>
2023-03-22 16:11:58 +0100

    Begynt API-integrasjon med Nowcast

---
+++ app/src/main/java/com/example/in2000_papirfly/data/NowcastData.kt
@@ -0,0 +1,48 @@
+package com.example.in2000_papirfly.data
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NowcastData (
+    val type: String,
+    val geometry: LocationGeometry,
+    val properties: NowcastProperties
+)
+
+@Serializable
+data class LocationGeometry (
+    val type: String,
+    val coordinates: List<Int>
+)
+
+@Serializable
+data class NowcastProperties (
+    val meta: NowcastMeta,
+    val timeseries: WeatherTimeseries
+)
+
+@Serializable
+data class NowcastMeta (
+    val updated_at: String, //sikkert bedre med et datoobjekt
+    val units: NowcastUnits
+)
+
+@Serializable
+data class NowcastUnits (
+    val air_temperature: String,
+    val precipitation_amount: String,
+    val precipitation_rate: String,
+    val relative_humidity: String,
+    val wind_from_direction: String,
+    val wind_speed: String,
+    val wind_speed_of_gusts: String
+)
+
+data class WeatherTimeseries (
+    val timeseries: List<WeatherTime>
+)
+
+data class WeatherTime (
+    val time: String,
+    val data: Object //TODO
+)
