313fb2767022048723e1534c56d9944238a4cf06
epicmoment <chrberge.berge@gmail.com>
2023-03-23 14:11:27 +0100

    Fikset merge-problemer, dependencies og positionscreen

--- app/src/main/java/com/example/in2000_papirfly/data/PositionScreenUiState.kt
+++ app/src/main/java/com/example/in2000_papirfly/data/PositionScreenUiState.kt
@@ -3,5 +3,5 @@ package com.example.in2000_papirfly.data
 data class PositionScreenUiState (
     val name: String = "Oslo",
     val icon: String = "Sol",
-    val data: NowcastData,
+    //val data: NowcastData,
 )
--- app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
+++ app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
@@ -4,21 +4,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.in2000_papirfly.data.Location
 import com.example.in2000_papirfly.ui.viewModels.PositionScreenViewModel
 
 @Composable
-fun PositionScreen(onNextPage : (Location) -> Unit,
+fun PositionScreen(
                    viewModel: PositionScreenViewModel = viewModel(),
                    modifier: Modifier = Modifier,
+                   onNextPage : (Location) -> Unit
 ) {
 
-    val posScrUiState by viewModel.posScrUiState.collectAsStateWithLifecycle()
+    val posScrUiState = viewModel.posScrUiState.collectAsState()
 
     Column(horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()) {
