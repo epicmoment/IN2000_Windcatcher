fcd70446b87106e7c0ca697f6b417a80b346f5f9
epicmoment <chrberge.berge@gmail.com>
2023-03-23 13:49:34 +0100

    La til et "skeleton" av MainScreen, la den til i navigation

--- app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
+++ app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
@@ -1,9 +1,23 @@
 package com.example.in2000_papirfly.ui.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.example.in2000_papirfly.data.Location
 
 @Composable
 fun PositionScreen(onNextPage : (Location) -> Unit) {
 
+    Text(text = "Position Screen wow!")
+    Button(
+        onClick = {
+            onNextPage(Location(0.0, 0.0))
+        }
+    ) {
+
+        Text("Neste side!")
+
+    }
 }
