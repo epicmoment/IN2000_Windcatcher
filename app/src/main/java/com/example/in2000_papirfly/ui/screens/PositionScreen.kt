91cc48cb6c68b1e3bfc1e7e4a350b36fcf00d1ac
Erlend <ermelleby@gmail.com>
2023-03-23 13:50:33 +0100

    Fikset merge problem på pos Screen

--- app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
+++ app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
@@ -1,9 +1,31 @@
 package com.example.in2000_papirfly.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.in2000_papirfly.data.Location
+import com.example.in2000_papirfly.ui.viewModels.PositionScreenViewModel
 
 @Composable
-fun PositionScreen(onNextPage : (Location) -> Unit) {
+fun PositionScreen(onNextPage : (Location) -> Unit,
+                   viewModel: PositionScreenViewModel = viewModel(),
+                   modifier: Modifier = Modifier,
+) {
 
+    val posScrUiState by viewModel.posScrUiState.collectAsStateWithLifecycle()
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()) {
+        LazyColumn(modifier = modifier
+            .fillMaxSize()) {
+            //items(posScrUiState.){
+            //}
+        }
+    }
 }
