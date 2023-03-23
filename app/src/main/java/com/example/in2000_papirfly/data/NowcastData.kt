893bf81a828e6ed3c58165b97fcae760d8d91e1b
Sivert Fjeldstad Madsen <sivertfm@gmail.com>
2023-03-23 14:14:21 +0100

    Fortsetter API-integrasjon med Nowcast...

--- app/src/main/java/com/example/in2000_papirfly/data/NowcastData.kt
+++ app/src/main/java/com/example/in2000_papirfly/data/NowcastData.kt
@@ -3,46 +3,81 @@ package com.example.in2000_papirfly.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class NowcastData (
-    val type: String,
-    val geometry: LocationGeometry,
-    val properties: NowcastProperties
+data class NowcastData(
+    val geometry: Geometry,
+    val properties: Properties,
+    val type: String
 )
 
 @Serializable
-data class LocationGeometry (
-    val type: String,
-    val coordinates: List<Int>
+data class Geometry(
+    val coordinates: List<Double>,
+    val type: String
 )
 
 @Serializable
-data class NowcastProperties (
-    val meta: NowcastMeta,
-    val timeseries: WeatherTimeseries
+data class Properties(
+    val meta: Meta,
+    val timeseries: List<WeatherTime>
 )
 
 @Serializable
-data class NowcastMeta (
-    val updated_at: String, //sikkert bedre med et datoobjekt
-    val units: NowcastUnits
+data class Meta(
+    val radar_coverage: String,
+    val units: Units,
+    val updated_at: String
 )
 
 @Serializable
-data class NowcastUnits (
+data class WeatherTime(
+    val data: Data,
+    val time: String
+)
+@Serializable
+
+data class Units(
     val air_temperature: String,
     val precipitation_amount: String,
     val precipitation_rate: String,
     val relative_humidity: String,
     val wind_from_direction: String,
     val wind_speed: String,
-    val wind_speed_of_gusts: String
+    val wind_speed_of_gust: String
 )
 
-data class WeatherTimeseries (
-    val timeseries: List<WeatherTime>
+@Serializable
+data class Data(
+    val instant: Instant,
+    val next_1_hours: Next1Hours
 )
 
-data class WeatherTime (
-    val time: String,
-    val data: Object //TODO
+@Serializable
+data class Instant(
+    val details: Details
+)
+
+@Serializable
+data class Next1Hours(
+    val details: DetailsX,
+    val summary: Summary
+)
+
+@Serializable
+data class Details(
+    val air_temperature: Double,
+    val precipitation_rate: Double,
+    val relative_humidity: Double,
+    val wind_from_direction: Double,
+    val wind_speed: Double,
+    val wind_speed_of_gust: Double
+)
+
+@Serializable
+data class DetailsX(
+    val precipitation_amount: Double
+)
+
+@Serializable
+data class Summary(
+    val symbol_code: String
 )
