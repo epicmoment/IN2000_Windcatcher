a3781afcbe855e7dc6a47041b9e8602ab3ec002e
Erlend <ermelleby@gmail.com>
2023-03-23 13:37:02 +0100

    Opprettet PosistionScreen, viewModel og UiState

--- app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
+++ app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
@@ -1,12 +1,30 @@
 package com.example.in2000_papirfly.ui.screens
 
+import android.location.Location
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.in2000_papirfly.ui.viewModels.PositionScreenViewModel
 
 @Composable
-fun PositionScreen(
+fun PositionScreen(onNextPage: (Location) -> Unit,
     viewModel: PositionScreenViewModel = viewModel(),
+    modifier: Modifier = Modifier,
+) {
 
     val posScrUiState by viewModel.posScrUiState.collectAsStateWithLifecycle()
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()) {
+        LazyColumn(modifier = modifier
+            .fillMaxSize()) {
+            items(posScrUiState.){
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/in2000_papirfly/ui/viewModels/PositionScreenViewModel.kt
+++ app/src/main/java/com/example/in2000_papirfly/ui/viewModels/PositionScreenViewModel.kt
@@ -3,18 +3,18 @@ package com.example.in2000_papirfly.ui.viewModels
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.in2000_papirfly.data.PositionScreenUiState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.IOException
-
 class PositionScreenViewModel: ViewModel() {
     //må ha PositionScreenUiState inni <>
     //PositionsScreenUiState må lages
-    private var _posScrUiState: MutableStateFlow<> = MutableStateFlow()
-    var posScrUiState: StateFlow<> = _posScrUiState.asStateFlow()
+    private var _posScrUiState: MutableStateFlow<PositionScreenUiState> = MutableStateFlow(PositionScreenUiState())
+    var posScrUiState: StateFlow<PositionScreenUiState> = _posScrUiState.asStateFlow()
 
     init {
         posScreenView()
