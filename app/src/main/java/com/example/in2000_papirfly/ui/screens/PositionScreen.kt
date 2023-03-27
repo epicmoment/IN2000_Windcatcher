4a5f57f2567840f4b99c100138d2a38097f14bc4
SaraAmundsen <sarahamundsen00@gmail.com>
2023-03-27 15:51:33 +0200

    tester farger

--- app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
+++ app/src/main/java/com/example/in2000_papirfly/ui/screens/PositionScreen.kt
@@ -1,8 +1,19 @@
 package com.example.in2000_papirfly.ui.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.in2000_papirfly.ui.NavScreen
+import com.example.in2000_papirfly.ui.theme.IN2000_PapirflyTheme
+import io.ktor.websocket.*
 
 @Composable
 fun PositionScreen() {
-
+    IN2000_PapirflyTheme {
+    }
 }
