9b727f315b1815b853a8f9b87fa1b70aefa0d136
Sivert Fjeldstad Madsen <sivertfm@gmail.com>
2023-03-29 23:51:04 +0200

    Laget weatherdummy og oppdatert kartcomposable-en litt

---
+++ app/src/main/java/com/example/in2000_papirfly/data/WeatherRepositoryDummy.kt
@@ -0,0 +1,13 @@
+package com.example.in2000_papirfly.data
+
+import kotlin.random.Random
+
+/**
+ * A dummy object with fairly random weather data.
+ */
+data class WeatherRepositoryDummy(
+    val temperature: Double = Random.nextDouble(-30.0, 30.0),
+    val windSpeed: Double = Random.nextDouble(0.0, 55.0),
+    val windFromDirection: Double = Random.nextDouble(0.0, 359.9),
+    val precipitationAmount: Double = Random.nextDouble(0.0, 50.0)
+    )
