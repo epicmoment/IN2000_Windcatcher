73e9fcfba3cdeec3ea8968691e05bd7cc72d8cf2
julian <julianfjeld@hotmail.com>
2023-03-30 15:34:36 +0200

    semi functional plane flying (map doesn't move)

--- app/src/main/java/com/example/in2000_papirfly/Plane/Plane.kt
+++ app/src/main/java/com/example/in2000_papirfly/Plane/Plane.kt
@@ -8,7 +8,7 @@ import kotlin.math.sin
 data class Plane(
     val flightModifier: FlightModifier = FlightModifier(),
     val pos : List<Double> = listOf(59.943325914913615, 10.717908529673489),
-    val launched : Boolean = false,
+    val flying : Boolean = false,
     val angle : Double = 45.0,  // given in degrees
     val speed : Double = 0.0,
     val height : Double = 100.0
