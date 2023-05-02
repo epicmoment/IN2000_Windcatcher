f147153fff5d975314ec26c8605f88eb7e908e94
julian <julianfjeld@hotmail.com>
2023-05-02 22:53:08 +0200

    La til kastelokasjoner

--- app/src/main/java/com/example/in2000_papirfly/data/ThrowPointList.kt
+++ app/src/main/java/com/example/in2000_papirfly/data/ThrowPointList.kt
@@ -12,5 +12,16 @@ object ThrowPointList {
             Pair("Nordkapp", GeoPoint(71.1705, 25.7842)),
             Pair("Lindesnes Fyr", GeoPoint(57.9828, 7.0466)),
             Pair("Drammen", GeoPoint(59.7439, 10.2049)),
+            Pair("Tromsø", GeoPoint(69.6797, 18.9091)),
+            Pair("Kya fyr", GeoPoint(64.4639, 10.2119)),
+            Pair("Kolvereid", GeoPoint(64.8650, 11.6046)),
+            Pair("Hammerfest", GeoPoint(70.6801, 23.6760)),
+            Pair("Henningsvær", GeoPoint(68.1508, 14.2006)),
+            Pair("Ålesund", GeoPoint(62.4577, 6.0884)),
+            Pair("Dombås", GeoPoint(62.0751, 9.1260)),
+            Pair("Vardø", GeoPoint(70.3870, 31.1687)),
+            Pair("Sandland", GeoPoint(70.2686, 21.6014)),
+            Pair("Hardanger", GeoPoint(60.1614, 7.4963)),
+            Pair("Bergen", GeoPoint(60.3803, 5.3568))
         )
 }
