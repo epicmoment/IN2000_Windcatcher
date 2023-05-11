c765d4f94011511bf8242bb10b1a17e77a2bc85e
julian <julianfjeld@hotmail.com>
2023-05-11 13:33:01 +0200

    WIP 02 - løfter ut vektorregning til et eget objekt og lager tester

--- app/src/main/java/com/example/in2000_papirfly/helpers/Vector.kt
+++ app/src/main/java/com/example/in2000_papirfly/helpers/Vector.kt
@@ -8,43 +8,53 @@ import kotlin.math.sqrt
 class Vector(
     val x: Double,
     val y: Double,
-    private val zeroDegreeAngle: Vector = Vector(1.0, 0.0),
 ){
     constructor(values: List<Double>): this(values[0], values[1])
 
-    fun calculateVector(angle: Double, magnitude: Double): Vector{
-        val radianAngle = Math.toRadians(angle)
-        val x = magnitude * cos(radianAngle)
-        val y = magnitude * cos(Math.toRadians(90.0) - radianAngle)
-        return Vector(x, y)
-    }
-
-    fun calculateAngle(vector1: Vector, vector2: Vector = zeroDegreeAngle): Double{
-        // returns the angle of a vector given in degrees
-        var newAngle =  Math.toDegrees( acos(dotProduct(vector1, vector2) / (vectorLength(vector1) * vectorLength(vector2))) )
-        if (vector1.y < 0){
-            newAngle = 360.0 - newAngle
+    operator fun get(index: Int): Double{
+        if (index == 0){
+            return x
         }
-        return newAngle
+        return y
     }
+    companion object {
 
-    fun dotProduct(vector1: Vector, vector2: Vector): Double{
-        return (vector1.x * vector2.y) + (vector1.x * vector2.y)
-    }
+        val zeroDegreeAngle: Vector = Vector(1.0, 0.0)
 
-    fun vectorLength(vector: Vector): Double{
-        return sqrt(vector.x.pow(2) + vector.y.pow(2))
-    }
+        fun calculateVector(angle: Double, magnitude: Double): Vector{
+            val radianAngle = Math.toRadians(angle)
+            val x = magnitude * cos(radianAngle)
+            val y = magnitude * cos(Math.toRadians(90.0) - radianAngle)
+            return Vector(x, y)
+        }
 
-    fun addVectors(vector1 : Vector, vector2 : Vector): Vector{
-        return Vector(vector1.x + vector2.x, vector1.y + vector2.y)
-    }
+        fun calculateAngle(vector1: Vector, vector2: Vector = zeroDegreeAngle): Double{
+            // returns the angle of a vector given in degrees
+            var newAngle =  Math.toDegrees( acos(dotProduct(vector1, vector2) / (vectorLength(vector1) * vectorLength(vector2))) )
+            if (vector1.y < 0){
+                newAngle = 360.0 - newAngle
+            }
+            return newAngle
+        }
 
-    fun subtractVectors(vector1: Vector, vector2: Vector): Vector{
-        return Vector(vector1.x - vector2.x, vector1.y - vector2.y)
-    }
+        fun dotProduct(vector1: Vector, vector2: Vector): Double{
+            return (vector1.x * vector2.y) + (vector1.x * vector2.y)
+        }
 
-    fun multiplyVector(vector1: Vector, x: Double): Vector{
-        return Vector(vector1.x * x, vector1.y * x)
+        fun vectorLength(vector: Vector): Double{
+            return sqrt(vector.x.pow(2) + vector.y.pow(2))
+        }
+
+        fun addVectors(vector1 : Vector, vector2 : Vector): Vector{
+            return Vector(vector1.x + vector2.x, vector1.y + vector2.y)
+        }
+
+        fun subtractVectors(vector1: Vector, vector2: Vector): Vector{
+            return Vector(vector1.x - vector2.x, vector1.y - vector2.y)
+        }
+
+        fun multiplyVector(vector1: Vector, x: Double): Vector{
+            return Vector(vector1.x * x, vector1.y * x)
+        }
     }
 }
